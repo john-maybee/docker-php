b75b632732b77b2507d352dfdb9f872a6b8d9495
John Maybee <john.d.maybee@gmail.com>
2023-09-14 21:55:14 -0400

    added some basic php to the index.php. Also, left a todo note

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,25 @@
 <?php
 
 echo 'Hello World';
+
+echo ' ';
+
+$name = 'John';
+
+$lastName = 'Maybee';
+
+$fullName = "$name $lastName";
+
+echo $fullName;
+?>
+
+<body>
+    <h1>Hello World!</h1>
+    <h2><?php echo $fullName ?></h2>
+    <p>My first paragraph.</p>
+</body>
+
+<?php
+
+
+?>
